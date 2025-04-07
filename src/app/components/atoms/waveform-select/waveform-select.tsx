e3df9cf7dac5f93914eafa29d9ea8a0ed4a7f5e5
acidok <dev@acidarchive.com>
2025-04-08 00:16:14 +0100

    feat: add waveform select component

--- src/app/components/atoms/waveform-select/waveform-select.tsx
+++ src/app/components/atoms/waveform-select/waveform-select.tsx
@@ -1,70 +1,56 @@
-export enum Waveform {
-  Square = 'square',
-  Sawtooth = 'sawtooth',
-}
-
+import { Radio, RadioGroup } from '@headlessui/react';
 import clsx from 'clsx';
 
 import { Sawtooth } from './sawtooth';
 import { Square } from './square';
 import styles from './waveform-select.module.scss';
 
+export enum Waveform {
+  Square = 'square',
+  Sawtooth = 'sawtooth',
+}
+
 export interface WaveformSelectProps {
   value?: Waveform;
+  defaultValue?: Waveform;
+  onChange?: (value: Waveform) => void;
   disabled?: boolean;
-  onChange: (value: Waveform) => void;
 }
 export const WaveformSelect = ({
   value,
-  disabled,
+  defaultValue = Waveform.Sawtooth,
   onChange,
+  disabled = false,
 }: WaveformSelectProps) => {
+  const handleChange = (selectedValue: Waveform) => {
+    onChange?.(selectedValue);
+  };
   return (
-    <div className={styles.waveformSelect}>
-      <span>Waveform:</span>
+    <RadioGroup
+      value={value || defaultValue}
+      className={clsx(styles.waveformSelect, { [styles.disabled]: disabled })}
+      disabled={disabled}
+      onChange={handleChange}
+    >
+      <div className={styles.label}>Waveform</div>
       <div className={styles.options}>
-        <label
-          className={clsx(styles.option, {
-            [styles.active]: value === Waveform.Square,
-            [styles.disabled]: disabled,
-          })}
+        <Radio
+          value={Waveform.Sawtooth}
+          className={({ checked }) =>
+            clsx(styles.option, { [styles.active]: checked })
+          }
         >
-          <span className={styles.input}>
-            <input
-              type="radio"
-              name="waveform"
-              value={Waveform.Square}
-              checked={value === Waveform.Square}
-              onChange={() => onChange(Waveform.Square)}
-              disabled={disabled}
-            />
-            <span className={styles.image}>
-              <Square />
-            </span>
-          </span>
-        </label>
-
-        <label
-          className={clsx(styles.option, {
-            [styles.active]: value === Waveform.Sawtooth,
-            [styles.disabled]: disabled,
-          })}
+          <Sawtooth />
+        </Radio>
+        <Radio
+          value={Waveform.Square}
+          className={({ checked }) =>
+            clsx(styles.option, { [styles.active]: checked })
+          }
         >
-          <span className={styles.input}>
-            <input
-              type="radio"
-              name="waveform"
-              value={Waveform.Sawtooth}
-              checked={value === Waveform.Sawtooth}
-              onChange={() => onChange(Waveform.Sawtooth)}
-              disabled={disabled}
-            />
-            <span className={styles.inputControl}>
-              <Sawtooth />
-            </span>
-          </span>
-        </label>
+          <Square />
+        </Radio>
       </div>
-    </div>
+    </RadioGroup>
   );
 };
