d9c90bb7a7af97f47ff0f7985cac3bb2aef15212
acidok <dev@acidarchive.com>
2025-04-14 04:38:02 +0100

    feat: add tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
 import '@/app/styles/index.scss';
+import '@/app/styles/globals.css';
 
 import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
 import { MainLayout } from '@/app/components/layouts/main-layout';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
   title: 'Acid Archive',
@@ -15,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body>
         <MainLayout>{children}</MainLayout>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from '@/app/styles/pages/HomePage.module.scss';
 export default function HomePage() {
   return (
     <div className={styles.content}>
-      <h2>303030303</h2>
+      <h2 className="font-sans text-3xl font-bold">303030303</h2>
     </div>
   );
 }
