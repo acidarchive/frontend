c09b40ff559627099e3f1b8b60f49550332894e0
acidok <dev@acidarchive.com>
2025-04-23 16:46:02 +0100

    feat: add error message component

---
+++ src/app/components/atoms/error-message/error-message.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ErrorMessage } from './error-message';
+
+const meta = {
+  title: 'Atoms/ErrorMessage',
+  component: ErrorMessage,
+  tags: ['autodocs'],
+} satisfies Meta<typeof ErrorMessage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    message: 'There was an error with your submission',
+  },
+};
