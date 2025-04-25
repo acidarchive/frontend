cc7dfb089b78ebc44d080c897d447a0c093ccbe8
acidok <dev@acidarchive.com>
2025-04-25 11:15:39 +0100

    fix: update button styling

--- src/app/components/atoms/button/button.tsx
+++ src/app/components/atoms/button/button.tsx
@@ -7,7 +7,7 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 export function Button({ children, ...props }: ButtonProps) {
   return (
     <button
-      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      className="flex w-full justify-center border border-gray-950 bg-gray-950 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-white hover:text-gray-950 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
       {...props}
     >
       {children}
