7d555976081a0629b53a2c96fa0fbad5c4fc17cc
acidok <dev@acidarchive.com>
2025-04-25 11:16:25 +0100

    fix: update input styling

--- src/app/components/atoms/input/input.tsx
+++ src/app/components/atoms/input/input.tsx
@@ -1,10 +1,31 @@
+import { ExclamationCircleIcon } from '@heroicons/react/16/solid';
+import clsx from 'clsx';
 import { InputHTMLAttributes } from 'react';
 
-export function Input({ ...props }: InputHTMLAttributes<HTMLInputElement>) {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean;
+}
+
+export function Input({ error, ...props }: InputProps) {
   return (
-    <input
-      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-      {...props}
-    />
+    <div className="mt-2 grid grid-cols-1">
+      <input
+        className={clsx(
+          'col-start-1 row-start-1 block w-full bg-white py-1.5 pr-10 pl-3 text-base outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 sm:pr-9 sm:text-sm/6',
+          {
+            'text-red-900 outline-red-300 placeholder:text-red-300 focus:outline-red-600':
+              error,
+            'text-gray-900 outline-gray-300 focus:outline-gray-950': !error,
+          },
+        )}
+        {...props}
+      />
+      {error && (
+        <ExclamationCircleIcon
+          aria-hidden="true"
+          className="pointer-events-none col-start-1 row-start-1 mr-3 size-5 self-center justify-self-end text-red-500 sm:size-4"
+        />
+      )}
+    </div>
   );
 }
