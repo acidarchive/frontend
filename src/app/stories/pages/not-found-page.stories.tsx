d907212e1dc1eab5c12b564585e85b39a04d860c
acidok <dev@acidarchive.com>
2025-05-20 23:54:40 +0100

    feat: 404 page

---
+++ src/app/stories/pages/not-found-page.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import NotFoundPage from '@/app/not-found';
+
+const meta = {
+  title: 'Pages/NotFoundPage',
+  component: NotFoundPage,
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof NotFoundPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
