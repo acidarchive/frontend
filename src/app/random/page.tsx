5a9aaf62426344c79e44110a04232cd5b04edbd0
acidok <dev@acidarchive.com>
2025-05-21 10:40:53 +0100

    feat: fetch random pattern in home page

--- src/app/random/page.tsx
+++ src/app/random/page.tsx
@@ -1,12 +1,15 @@
-'use client';
-
 import { YoutubePlayer } from '@/app/components/atoms/youtube-player';
 import { MainLayout } from '@/app/components/layouts/main-layout';
 import { Comments } from '@/app/components/organisms/comments';
-import { pattern, PatternTB303 } from '@/app/components/organisms/pattern';
+import { PatternTB303 } from '@/app/components/organisms/pattern';
 import { PatternDetails } from '@/app/components/organisms/pattern-details';
 
-export default function PatternPage() {
+export default async function PatternPage() {
+  const data = await fetch(
+    `${process.env.NEXT_PUBLIC_API_HOST}/v1/patterns/tb303/random`,
+  );
+  const response = await data.json();
+
   return (
     <MainLayout>
       <main>
@@ -18,13 +21,16 @@ export default function PatternPage() {
             </div>
 
             <div className="-mx-4 sm:mx-0 lg:col-span-2 lg:row-span-2 lg:row-end-2 flex flex-col gap-y-8">
-              <PatternTB303 pattern={pattern} />
+              <PatternTB303 pattern={response.data} />
               <div>
                 <Comments />
               </div>
             </div>
 
             <div className="lg:col-start-3">
+              <h2 className="text-sm/6 font-semibold text-gray-900 mb-4">
+                Videos
+              </h2>
               <YoutubePlayer />
             </div>
           </div>
