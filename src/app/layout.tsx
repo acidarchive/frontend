b7a5df0982e487cc5f8148fcfa9ee5dc1becdfef
acidok <dev@acidarchive.com>
2025-07-08 18:47:05 +0100

    fix: move providers to src

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { Inter } from 'next/font/google';
 import { NuqsAdapter } from 'nuqs/adapters/next/app';
 
 import ConfigureAmplifyClientSide from '@/app/amplify-cognito-config';
-import QueryProvider from '@/app/providers/query-provider';
 import { UserProvider } from '@/context/user-context';
+import QueryProvider from '@/providers/query-provider';
 
 const inter = Inter({
   subsets: ['latin'],
