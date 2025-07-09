f8676b8f5d4268e29d94c0b2fbde1d189d5737d3
acidok <dev@acidarchive.com>
2025-07-09 11:42:22 +0100

    chore: generate types

--- src/api/generated/acid.ts
+++ src/api/generated/acid.ts
@@ -3,7 +3,7 @@
  * Do not edit manually.
  * acid
  * API for acidarchive.com
- * OpenAPI spec version: 0.0.1-alpha.4
+ * OpenAPI spec version: 0.0.1-alpha.8
  */
 import {
   useMutation,
@@ -263,3 +263,144 @@ export function useGetRandomTb303Pattern<TData = Awaited<ReturnType<typeof getRa
 
 
 
+export const getTb303Pattern = (
+    patternId: string,
+ options?: SecondParameter<typeof customInstance>,signal?: AbortSignal
+) => {
+      
+      
+      return customInstance<TB303Pattern>(
+      {url: `/v1/patterns/tb303/${patternId}`, method: 'GET', signal
+    },
+      options);
+    }
+  
+
+export const getGetTb303PatternQueryKey = (patternId: string,) => {
+    return [`/v1/patterns/tb303/${patternId}`] as const;
+    }
+
+    
+export const getGetTb303PatternQueryOptions = <TData = Awaited<ReturnType<typeof getTb303Pattern>>, TError = void>(patternId: string, options?: { query?:Partial<UseQueryOptions<Awaited<ReturnType<typeof getTb303Pattern>>, TError, TData>>, request?: SecondParameter<typeof customInstance>}
+) => {
+
+const {query: queryOptions, request: requestOptions} = options ?? {};
+
+  const queryKey =  queryOptions?.queryKey ?? getGetTb303PatternQueryKey(patternId);
+
+  
+
+    const queryFn: QueryFunction<Awaited<ReturnType<typeof getTb303Pattern>>> = ({ signal }) => getTb303Pattern(patternId, requestOptions, signal);
+
+      
+
+      
+
+   return  { queryKey, queryFn, enabled: !!(patternId), ...queryOptions} as UseQueryOptions<Awaited<ReturnType<typeof getTb303Pattern>>, TError, TData> & { queryKey: DataTag<QueryKey, TData, TError> }
+}
+
+export type GetTb303PatternQueryResult = NonNullable<Awaited<ReturnType<typeof getTb303Pattern>>>
+export type GetTb303PatternQueryError = void
+
+
+export function useGetTb303Pattern<TData = Awaited<ReturnType<typeof getTb303Pattern>>, TError = void>(
+ patternId: string, options: { query:Partial<UseQueryOptions<Awaited<ReturnType<typeof getTb303Pattern>>, TError, TData>> & Pick<
+        DefinedInitialDataOptions<
+          Awaited<ReturnType<typeof getTb303Pattern>>,
+          TError,
+          Awaited<ReturnType<typeof getTb303Pattern>>
+        > , 'initialData'
+      >, request?: SecondParameter<typeof customInstance>}
+ , queryClient?: QueryClient
+  ):  DefinedUseQueryResult<TData, TError> & { queryKey: DataTag<QueryKey, TData, TError> }
+export function useGetTb303Pattern<TData = Awaited<ReturnType<typeof getTb303Pattern>>, TError = void>(
+ patternId: string, options?: { query?:Partial<UseQueryOptions<Awaited<ReturnType<typeof getTb303Pattern>>, TError, TData>> & Pick<
+        UndefinedInitialDataOptions<
+          Awaited<ReturnType<typeof getTb303Pattern>>,
+          TError,
+          Awaited<ReturnType<typeof getTb303Pattern>>
+        > , 'initialData'
+      >, request?: SecondParameter<typeof customInstance>}
+ , queryClient?: QueryClient
+  ):  UseQueryResult<TData, TError> & { queryKey: DataTag<QueryKey, TData, TError> }
+export function useGetTb303Pattern<TData = Awaited<ReturnType<typeof getTb303Pattern>>, TError = void>(
+ patternId: string, options?: { query?:Partial<UseQueryOptions<Awaited<ReturnType<typeof getTb303Pattern>>, TError, TData>>, request?: SecondParameter<typeof customInstance>}
+ , queryClient?: QueryClient
+  ):  UseQueryResult<TData, TError> & { queryKey: DataTag<QueryKey, TData, TError> }
+
+export function useGetTb303Pattern<TData = Awaited<ReturnType<typeof getTb303Pattern>>, TError = void>(
+ patternId: string, options?: { query?:Partial<UseQueryOptions<Awaited<ReturnType<typeof getTb303Pattern>>, TError, TData>>, request?: SecondParameter<typeof customInstance>}
+ , queryClient?: QueryClient 
+ ):  UseQueryResult<TData, TError> & { queryKey: DataTag<QueryKey, TData, TError> } {
+
+  const queryOptions = getGetTb303PatternQueryOptions(patternId,options)
+
+  const query = useQuery(queryOptions , queryClient) as  UseQueryResult<TData, TError> & { queryKey: DataTag<QueryKey, TData, TError> };
+
+  query.queryKey = queryOptions.queryKey ;
+
+  return query;
+}
+
+
+
+
+export const updateTb303Pattern = (
+    patternId: string,
+    createTB303Pattern: CreateTB303Pattern,
+ options?: SecondParameter<typeof customInstance>,) => {
+      
+      
+      return customInstance<PatternTB303Response>(
+      {url: `/v1/patterns/tb303/${patternId}`, method: 'PUT',
+      headers: {'Content-Type': 'application/json', },
+      data: createTB303Pattern
+    },
+      options);
+    }
+  
+
+
+export const getUpdateTb303PatternMutationOptions = <TError = void,
+    TContext = unknown>(options?: { mutation?:UseMutationOptions<Awaited<ReturnType<typeof updateTb303Pattern>>, TError,{patternId: string;data: CreateTB303Pattern}, TContext>, request?: SecondParameter<typeof customInstance>}
+): UseMutationOptions<Awaited<ReturnType<typeof updateTb303Pattern>>, TError,{patternId: string;data: CreateTB303Pattern}, TContext> => {
+
+const mutationKey = ['updateTb303Pattern'];
+const {mutation: mutationOptions, request: requestOptions} = options ?
+      options.mutation && 'mutationKey' in options.mutation && options.mutation.mutationKey ?
+      options
+      : {...options, mutation: {...options.mutation, mutationKey}}
+      : {mutation: { mutationKey, }, request: undefined};
+
+      
+
+
+      const mutationFn: MutationFunction<Awaited<ReturnType<typeof updateTb303Pattern>>, {patternId: string;data: CreateTB303Pattern}> = (props) => {
+          const {patternId,data} = props ?? {};
+
+          return  updateTb303Pattern(patternId,data,requestOptions)
+        }
+
+        
+
+
+  return  { mutationFn, ...mutationOptions }}
+
+    export type UpdateTb303PatternMutationResult = NonNullable<Awaited<ReturnType<typeof updateTb303Pattern>>>
+    export type UpdateTb303PatternMutationBody = CreateTB303Pattern
+    export type UpdateTb303PatternMutationError = void
+
+    export const useUpdateTb303Pattern = <TError = void,
+    TContext = unknown>(options?: { mutation?:UseMutationOptions<Awaited<ReturnType<typeof updateTb303Pattern>>, TError,{patternId: string;data: CreateTB303Pattern}, TContext>, request?: SecondParameter<typeof customInstance>}
+ , queryClient?: QueryClient): UseMutationResult<
+        Awaited<ReturnType<typeof updateTb303Pattern>>,
+        TError,
+        {patternId: string;data: CreateTB303Pattern},
+        TContext
+      > => {
+
+      const mutationOptions = getUpdateTb303PatternMutationOptions(options);
+
+      return useMutation(mutationOptions , queryClient);
+    }
+    
