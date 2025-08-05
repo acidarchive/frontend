7a9de02750c4cc024fa4d796cfe5f2763ae392c9
acidok <dev@acidarchive.com>
2025-08-05 16:47:46 +0100

    fix: enable dynamic rendering

--- src/app/random/page.tsx
+++ src/app/random/page.tsx
@@ -1,6 +1,8 @@
 import { getRandomTB303Pattern } from '@/dal';
 import { RandomTB303Pattern } from '@/features/random-pattern';
 
+export const dynamic = 'force-dynamic';
+
 export default async function RandomPatternPage() {
   const pattern = await getRandomTB303Pattern();
 
