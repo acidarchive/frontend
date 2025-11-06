c161ec3571efbfd87220826e4bc2cfdcfd288033
acidok <dev@acidarchive.com>
2025-11-06 16:56:32 +0000

    feat: make dark mode default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
       <body className="h-full flex flex-col">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
