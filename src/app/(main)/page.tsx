bc29393d4f03c92715c8c65a93b1272e9c360e4a
acidok <dev@acidarchive.com>
2026-04-20 15:21:22 +0100

    feat: add metadata to home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,25 @@
+import type { Metadata } from 'next';
+
 import { listPublicPatternsTB303Server } from '@/dal';
 import { PublicPatternList } from '@/features/public-pattern-list/public-pattern-list';
+
+export const metadata: Metadata = {
+  title: 'Acid Archive',
+  description: 'Browse, share, explore and backup TB-303 patterns.',
+  openGraph: {
+    title: 'Acid Archive',
+    description: 'Browse, share, explore and backup TB-303 patterns.',
+    url: 'https://acidarchive.com',
+    siteName: 'Acid Archive',
+    type: 'website',
+    images: [
+      {
+        url: 'https://acidarchive.com/smiley',
+        alt: 'Acid Archive',
+      },
+    ],
+  },
+};
 
 const LIMIT = 30;
 
@@ -13,5 +33,7 @@ export default async function HomePage({ searchParams }: HomePageProps) {
   const offset = (currentPage - 1) * LIMIT;
   const data = await listPublicPatternsTB303Server(LIMIT, offset);
 
-  return <PublicPatternList data={data} currentPage={currentPage} limit={LIMIT} />;
+  return (
+    <PublicPatternList data={data} currentPage={currentPage} limit={LIMIT} />
+  );
 }
